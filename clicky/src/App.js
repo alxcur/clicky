270746fe3664c80be388f3069e6ef3e4c47e67dc
Alex Curington <alex.curington@me.com>
2019-04-27 10:55:26 -0500

    no message

---
+++ clicky/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      {/* The rest of your application */}
+    </React.Fragment>
+  );
+}
+
+export default App;
